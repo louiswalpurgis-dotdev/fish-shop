aacd3e9ec7ca0c5aee6cd8bb208f06565c56a1de
lilwxs <124787909+lilwxs@users.noreply.github.com>
2023-04-02 19:03:23 +0700

    ProductCard

--- frontend/src/components/Layout/components/ProductCard/ProductCard.js
+++ frontend/src/components/Layout/components/ProductCard/ProductCard.js
@@ -1,4 +1,4 @@
-import { Card, Col, Grid, Link, Row, Text, Loading } from '@nextui-org/react';
+import { Card, Col, Grid, Link, Row, Text, Loading, Button } from '@nextui-org/react';
 import { useState, useEffect } from 'react';
 import { Link as reactLink } from 'react-router-dom';
 import axios from '~/api/axios';
@@ -18,78 +18,98 @@ function ProductCard() {
     }, []);
     const MockItem = ({ image, name, amount, info, price, slug }) => {
         return (
-            // <Card css={{ $$cardColor: '$colors$primary' }}>
-            //     <Card.Body>
-            //         <Row justify="center" align="center">
-            //             <Image objectFit="cover" src={image}></Image>
-            //         </Row>
-            //         <Row>
-            //             <Text h4 size={20} css={{ m: 0 }}>
-            //                 {name}
-            //             </Text>
-            //             <Text h4 size={20} css={{ m: 0 }}>
-            //                 {amount}
-            //             </Text>
-            //         </Row>
-            //         <Row>
-            //             <Text h4 size={20} css={{ m: 0 }}>
-            //                 {info}
-            //             </Text>
-            //         </Row>
-            //         <Row>
-            //             <Text h4 size={20} css={{ m: 0 }}>
-            //                 {price}
-            //             </Text>
-            //         </Row>
-            //     </Card.Body>
-            // </Card>
-            <Card variant="none" borderWeight="none">
-                <Link as={reactLink} to={`/product/${slug}`}>
-                    <Card.Body>
-                        <Card.Image
-                            src={image}
-                            width="100%"
-                            height="200px"
-                            objectFit="cover"
-                            css={{ borderRadius: 14 }}
-                            alt={name}
-                        />
-                        <Row css={{ pt: 10, pb: 10 }}>
+            <Link as={reactLink} to={`/product/${slug}`}>
+                {/* <Card css={{ width: '100%', aspectRatio: '1/1', borderRadius: 14 }}>
+                    <Card.Image src={image} objectFit="cover" alt={name} />
+                    <Card.Body css={{ pt: 10, pb: 10 }}>
+                        <Text
+                            b
+                            size={16}
+                            css={{
+                                textGradient: '45deg, $blue600 -20%, $pink600 50%',
+                            }}
+                        >
+                            {name}
+                        </Text>
+
+                        <Row>
                             <Col>
-                                <Text
-                                    b
-                                    size={16}
-                                    css={{
-                                        textGradient: '45deg, $blue600 -20%, $pink600 50%',
-                                    }}
-                                >
-                                    {name}
+                                <Text size={14} color="black">
+                                    Số lương: {amount}
                                 </Text>
-                                <Text size={14} color="black" className="truncate line-clamp-3">
-                                    {info}
+                            </Col>
+                            <Col>
+                                <Text size={14} color="black" css={{ textAlign: 'right' }}>
+                                    Giá: {price}
                                 </Text>
-                                <Row>
-                                    <Col>
-                                        <Text size={14} color="black">
-                                            Số lương: {amount}
-                                        </Text>
-                                    </Col>
-                                    <Col>
-                                        <Text size={14} color="black" css={{ textAlign: 'right' }}>
-                                            Giá: {price}
-                                        </Text>
-                                    </Col>
-                                </Row>
                             </Col>
                         </Row>
                     </Card.Body>
-                </Link>
-            </Card>
+                </Card> */}
+                <Card>
+                    <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+                        <Col>
+                            <Text
+                                span
+                                size={12}
+                                weight="bold"
+                                color="#ffffffAA"
+                                css={{ bgBlur: '#ffffff66', py: 5, px: 10, borderRadius: 20 }}
+                            >
+                                {price} đ
+                            </Text>
+                            <Text h3 color="#ffffffAA">
+                                CATEGORY
+                            </Text>
+                        </Col>
+                    </Card.Header>
+                    <Card.Body css={{ p: 0 }}>
+                        <Card.Image src={image} width="100%" height="20rem" objectFit="cover" alt={name} />
+                    </Card.Body>
+                    <Card.Footer
+                        isBlurred
+                        css={{
+                            position: 'absolute',
+                            bgBlur: '#ffffff66',
+                            borderTop: '$borderWeights$light solid rgba(255, 255, 255, 0.2)',
+                            bottom: 0,
+                            zIndex: 1,
+                        }}
+                    >
+                        <Col>
+                            <Row>
+                                <Row>
+                                    <Text h6 color="#ffffffAA">
+                                        {name}
+                                    </Text>
+                                </Row>
+                                <Row justify="flex-end">
+                                    <Button flat auto rounded color="secondary">
+                                        <Text
+                                            css={{
+                                                textGradient: '45deg, $blue600 -20%, $pink600 50%',
+                                            }}
+                                            size={12}
+                                            weight="bold"
+                                            transform="uppercase"
+                                        >
+                                            MUA NGÀY
+                                        </Text>
+                                    </Button>
+                                </Row>
+                            </Row>
+                            <Text color="#ffffffAA" size={12}>
+                                {info}
+                            </Text>
+                        </Col>
+                    </Card.Footer>
+                </Card>
+            </Link>
         );
     };
     return (
         <Grid.Container gap={2} justify="flex-start">
-            {(loading == true && <Loading type="default" size="xl" style={{ margin: 'auto' }} />) ||
+            {(loading === true && <Loading type="default" size="xl" style={{ margin: 'auto' }} />) ||
                 data.map((item) => {
                     return (
                         <Grid xs={6} sm={4} md={3} lg={3} xl={2} key={item.id}>
