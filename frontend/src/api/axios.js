0b691ee2f3949e2100e2b2538394524704304195
MaiTy53392 <89622955+MaiTy53392@users.noreply.github.com>
2023-04-26 07:42:49 +0700

    UPDATE backend api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://fishshop-6ygq.onrender.com',
+    baseURL: 'https://fishshop-4y4l.onrender.com',
     headers: { 'Access-Control-Allow-Origin': '*' },
 });
